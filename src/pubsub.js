b87c4e04fcdf5f5feccba1dbc974692e2b57e641
Morgan Roderick <morgan@roderick.dk>
2012-04-01 08:04:34 +0200

    Updating copyright statement and license in top of pubsub.js

--- src/pubsub.js
+++ src/pubsub.js
@@ -1,27 +1,14 @@
 /*
-Copyright (c) 2010 Morgan Roderick http://roderick.dk
-
-Permission is hereby granted, free of charge, to any person obtaining
-a copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
-
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+Copyright (c) 2010,2011,2012 Morgan Roderick http://roderick.dk
+License: MIT – http://mrgnrdrck.mit-license.org
 */
 /*jslint white:true, plusplus:true */
-/*global setTimeout */
+/*global
+	setTimeout,
+	module,
+	exports,
+	define
+*/
 /** section: PubSub
  *	PubSubJS is a dependency free library for doing ['publish/subscribe'](http://en.wikipedia.org/wiki/Publish/subscribe)
  *	messaging in JavaScript.
