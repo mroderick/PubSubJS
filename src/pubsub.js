bbdccaa527bdfc723647549299c826af8ac4a417
sbusse <s.busse-extern@fielmann.com>
2020-01-28 15:47:31 +0100

    use block comments for lint

--- src/pubsub.js
+++ src/pubsub.js
@@ -14,10 +14,10 @@
     factory(PubSub);
 
     // AMD support
-    // eslint-disable no-undef
+    /* eslint-disable no-undef */
     if (typeof define === 'function' && define.amd){
         define(function() { return PubSub; });
-        // eslint-enable no-undef
+        /* eslint-enable no-undef */
 
         // CommonJS and Node.js module support
     } else if (typeof exports === 'object'){
